aa6b2b9d33a2b79cb6f703158388b62f2d30796d
Xiang Zhang <xiangzhang@Xiangs-MacBook-Pro.local>
2017-08-29 20:56:28 -0300

    update

--- LeetCode/No.1-No.99/leetCode(Q121-130)/Q125.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        str1 = [item for item in s.lower() if item.isalnum()]
-        return str1 == str1[::-1]
-print Solution().isPalindrome("A man, a plan, a canal: Panama")
-print Solution().isPalindrome("race a car")
-print Solution().isPalindrome("0P")
-print Solution().isPalindrome(" ")
-print Solution().isPalindrome("..   ")
